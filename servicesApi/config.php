90bd2914508b80f98151efdb07c5951f4d20882f
Miguel Tirador <82255624+mtiradorc@users.noreply.github.com>
2022-09-21 10:22:10 -0500

    Add services applications
    Add services applications

---
+++ servicesApi/config.php
@@ -0,0 +1,8 @@
+<?php
+$db = [
+    'host' => '192.168.1.16',
+    'username' => 'myuser',
+    'password' => 'myuser2649',
+    'db' => 'db_springboot_backend' //Cambiar al nombre de tu base de datos
+];
+?>
